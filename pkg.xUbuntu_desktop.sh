4849b91f359d9dc5f2fb4cb5724cfc32036b1763
Johan Simonsson <github@fun-tech.se>
2014-02-13 08:35:11 +0100

    More misc tools that I use

--- pkg.xUbuntu_desktop.sh
+++ pkg.xUbuntu_desktop.sh
@@ -8,9 +8,17 @@ apt-get upgrade || exit 11
 apt-get install \
     build-essential \
     vim vim-gtk exuberant-ctags cscope \
+    diffuse meld \
+    gdb ddd \
     git gitk \
     qt4-qmake qt4-dev-tools \
     valgrind \
+    doxygen graphviz \
+    gnuplot gnuplot-x11 \
+    wget curl \
+    visual-regexp xclip \
+    okteta ghex \
+    ckermit \
     openjdk-7-jre || exit 12
 
 echo "Done..."
