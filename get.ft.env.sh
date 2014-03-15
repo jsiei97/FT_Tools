89c7164789105257818fc25d61d849d0928df00d
Johan Simonsson <github@fun-tech.se>
2014-03-15 11:16:39 +0100

    How to get .vim and git_bin

---
+++ get.ft.env.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+projectdir=$HOME
+
+function get_git
+{
+    user=$1
+    git=$2
+    if [ -d $git ]
+    then
+        echo "No action: $git"
+    else
+        git clone git@github.com:$user/$git".git" $git || exit 20
+    fi
+}
+
+#mkdir -p $projectdir
+pushd $projectdir || exit 10
+
+get_git jsiei97 .vim
+
+if [ ! -e .vimrc ]
+then
+    echo "Add .vimrc"
+    cp .vim/.vimrc . || exit 20
+fi
+
+get_git jsiei97 git_bin
+
+if [ "$(grep git_bin ~/.bashrc)" ]
+then
+    echo "git_bin is in path"
+else
+    echo '' >> ~/.bashrc
+    echo 'export PATH=$PATH:~/git_bin' >> ~/.bashrc
+    echo '' >> ~/.bashrc
+fi
+
