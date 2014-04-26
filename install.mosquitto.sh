b2fac1a1c924009fda61d0323dda80dc95abb226
Johan Simonsson <github@fun-tech.se>
2014-04-26 18:28:47 +0000

    Add start and monit control

--- install.mosquitto.sh
+++ install.mosquitto.sh
@@ -32,5 +32,25 @@ pushd $version || exit 22
 make ||  exit 30
 sudo make install || exit 31
 
+sudo cp service/monit/mosquitto.monit /etc/monit/conf.d/mosquitto || exit 40
+
+popd
+
+if [ ! -f /etc/init.d/mosquitto ]
+then
+    echo "Fixing: /etc/init.d/mosquitto"
+    wget https://bitbucket.org/oojah/mosquitto-packaging/raw/1648d3dfd80b672abdc5d473d824620729cb4513/debian/mosquitto.init || exit 50
+    sed -i 's/^DAEMON=.*/DAEMON=\/usr\/local\/sbin\/mosquitto/g' mosquitto.init || exit 51
+    sudo cp mosquitto.init /etc/init.d/mosquitto || exit 52
+    sudo chmod 755 /etc/init.d/mosquitto || exit 54
+fi
+
+if [ ! -f /etc/mosquitto/mosquitto.conf ]
+then
+    echo "Fixing: /etc/mosquitto/mosquitto.conf"
+    sudo cp /etc/mosquitto/mosquitto.conf.example /etc/mosquitto/mosquitto.conf || exit 60
+    sudo sed -i 's/#user .*/user '$USER'/g' /etc/mosquitto/mosquitto.conf  || exit 62
+fi
+
 echo "Done..."
 exit 0
