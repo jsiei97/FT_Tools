5e74c72a794000d4f608fb9d9edc84718fb6dd34
Johan Simonsson <github@fun-tech.se>
2014-09-27 09:40:36 +0200

    Whitespace

--- install.arduino.sh
+++ install.arduino.sh
@@ -5,12 +5,12 @@ dest=/opt/arduino
 
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-	# 64-bit stuff here
-	#url=http://arduino.googlecode.com/files/arduino-1.0.5-linux64.tgz
+    # 64-bit stuff here
+    #url=http://arduino.googlecode.com/files/arduino-1.0.5-linux64.tgz
     url=http://downloads.arduino.cc/arduino-1.0.6-linux64.tgz
 else
-	# 32-bit stuff here
-	#url=http://arduino.googlecode.com/files/arduino-1.0.5-linux32.tgz
+    # 32-bit stuff here
+    #url=http://arduino.googlecode.com/files/arduino-1.0.5-linux32.tgz
     url=http://downloads.arduino.cc/arduino-1.0.6-linux32.tgz
 fi
 
@@ -18,17 +18,17 @@ name=$(basename $url)
 
 if [ ! -d "$dest" ]
 then
-	sudo mkdir -p $dest    || exit 10
-	sudo chown $USER $dest || exit 11
+    sudo mkdir -p $dest    || exit 10
+    sudo chown $USER $dest || exit 11
 fi
 
 cd $dest || exit 20
 
 if [ ! -f "$name" ]
 then
-	echo "Download file $name"
-	echo $url -O $name
-	wget $url -O $name || exit 21
+    echo "Download file $name"
+    echo $url -O $name
+    wget $url -O $name || exit 21
 fi
 
 tar xzf $name || exit 30
@@ -46,7 +46,7 @@ ln -s $shortname current || exit 36
 # Add ~/bin/arduino
 if [ -f "$target" ]
 then
-	echo ok
+    echo ok
     mkdir -p ~/bin/
     cd ~/bin || exit 40
 
