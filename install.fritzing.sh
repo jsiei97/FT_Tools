e0ec13db223906cc97b78cba3b038ae0ee9b3617
Johan Simonsson <github@fun-tech.se>
2014-09-27 09:39:23 +0200

    Use fritzing-0.9.0b

--- install.fritzing.sh
+++ install.fritzing.sh
@@ -1,43 +1,56 @@
 #!/bin/bash
 
+#For more info about
+# http://fritzing.org/
+# https://github.com/fritzing
+
 #Installation dir
 dest=/opt/fritzing
 
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-	# 64-bit stuff here
-	url=http://fritzing.org/download/0.8.7b/linux-64bit/fritzing-0.8.7b.linux.AMD64.tar.bz2
+    # 64-bit stuff here
+    #url=http://fritzing.org/download/0.8.7b/linux-64bit/fritzing-0.8.7b.linux.AMD64.tar.bz2
+    url=http://fritzing.org/download/0.9.0b/linux-64bit/fritzing-0.9.0b.linux.AMD64.tar_1.bz2
 else
-	# 32-bit stuff here
-	url=http://fritzing.org/download/0.8.7b/linux-32bit/fritzing-0.8.7b.linux.i386.tar.bz2
+    # 32-bit stuff here
+    #url=http://fritzing.org/download/0.8.7b/linux-32bit/fritzing-0.8.7b.linux.i386.tar.bz2
+    url=http://fritzing.org/download/0.9.0b/linux-32bit/fritzing-0.9.0b.linux.i386.tar_1.bz2
 fi
 
 name=$(basename $url)
 
 if [ ! -d "$dest" ]
 then
-	sudo mkdir -p $dest    || exit 10
-	sudo chown $USER $dest || exit 11
+    sudo mkdir -p $dest    || exit 10
+    sudo chown $USER $dest || exit 11
 fi
 
 cd $dest || exit 20
 
 if [ ! -f "$name" ]
 then
-	echo "Download file $name"
-	echo $url -O $name
-	wget $url -O $name || exit 21
+    echo "Download file $name"
+    echo $url -O $name
+    wget $url -O $name || exit 21
 fi
 
 tar xjf $name || exit 30
 
-shortname=$(basename $name .tar.bz2)
+#Why add something like _1 in the name???
+shortname=$(basename $name .tar_1.bz2)
 target="$PWD/$shortname/Fritzing"
+
 if [ -f "$target" ]
 then
     echo ok
     mkdir -p ~/bin/
     cd ~/bin || exit 40
+
+    if [ -f fritzing ]
+    then
+        rm fritzing
+    fi
     ln -s $target fritzing || exit 41
 fi
 
