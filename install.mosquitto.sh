9851e573121f41a7f5144895e18ab9718eeff725
Johan Simonsson <github@fun-tech.se>
2015-04-12 16:05:18 +0000

    Install with dpkg (libwebsockets.deb and mosquitto.deb)
    The build logic is now in those two gits,
    that more or less builds and creates a .deb
    and uses dpkg to install (and not make install)...

--- install.mosquitto.sh
+++ install.mosquitto.sh
@@ -5,66 +5,35 @@ project=~/funtech/mosquitto/
 mkdir -p $project/
 pushd $project || exit 10
 
-# First fix a libwebsockets that works with mosq...
-if [ -d libwebsockets ]
+# First fix a libwebsockets that works with newer mosq...
+echo "About to build libwebsockets"
+
+if [ -d libwebsockets.deb ]
 then
-    rm -rf libwebsockets || exit 20
+    rm -rf libwebsockets.deb || exit 20
 fi
-git clone git@github.com:jsiei97/libwebsockets.git libwebsockets || exit 22
 
-mkdir -p libwebsockets/build
-pushd libwebsockets/build || exit 24
+git clone git@github.com:jsiei97/libwebsockets.deb.git || exit 22
 
-cmake ..          || exit 26
-make              || exit 27
-sudo make install || exit 28
-
+pushd libwebsockets.deb || exit 23
+git submodule update --init --recursive || exit 24
+./build.sh || exit $?
 popd || exit 29
 
-
 # Then build mosquitto 1.4 with websockets
-if [ -d mosquitto_1.4 ]
+echo "About to build mosquitto"
+
+if [ -d mosquitto.deb ]
 then
-    rm -rf mosquitto_1.4 || exit 80
+    rm -rf mosquitto.deb || exit 80
 fi
 
-git clone -b 1.4 http://git.eclipse.org/gitroot/mosquitto/org.eclipse.mosquitto.git mosquitto_1.4 || exit 82
+git clone git@github.com:jsiei97/mosquitto.deb.git mosquitto.deb || exit 82
 
-pushd mosquitto_1.4/ || exit 84
-sed -i 's/\(WITH_WEBSOCKETS:=\).*/\1yes/g' config.mk || exit 85
-
-make ||  exit 30
-sudo make install || exit 31
-
-sudo cp service/monit/mosquitto.monit /etc/monit/conf.d/mosquitto || exit 40
-
-popd || exit 89
-
-if [ ! -f /etc/init.d/mosquitto ]
-then
-    echo "Fixing: /etc/init.d/mosquitto"
-    wget https://bitbucket.org/oojah/mosquitto-packaging/raw/1648d3dfd80b672abdc5d473d824620729cb4513/debian/mosquitto.init || exit 50
-    sed -i 's/^DAEMON=.*/DAEMON=\/usr\/local\/sbin\/mosquitto/g' mosquitto.init || exit 51
-    sudo cp mosquitto.init /etc/init.d/mosquitto || exit 52
-    sudo chmod 755 /etc/init.d/mosquitto || exit 54
-fi
-
-if [ ! -f /etc/mosquitto/mosquitto.conf ]
-then
-    echo "Fixing: /etc/mosquitto/mosquitto.conf"
-
-cat > mosquitto.conf << EOF
-# Add both normal mqtt on port 1883
-# and websockets on port 9001
-listener 1883
-listener 9001 127.0.0.1
-protocol websockets
-
-EOF
-    cat /etc/mosquitto/mosquitto.conf.example >> mosquitto.conf || exit 60
-    sed -i 's/#user .*/user '$USER'/g' mosquitto.conf || exit 62
-    sudo cp mosquitto.conf /etc/mosquitto/mosquitto.conf || exit 64
-fi
+pushd mosquitto.deb || exit 84
+git submodule update --init --recursive || exit 86
+./build.sh || exit $?
+popd
 
 echo "Done..."
 exit 0
