deb95906f0aae3a21bc1a8eacd576fcfb9e166dd
Johan <github@fun-tech.se>
2022-09-13 08:34:25 +0200

    Out of date, will update names later

--- pkg.raspian.mosqhub.sh
+++ pkg.raspian.mosqhub.sh
@@ -7,16 +7,16 @@ apt -y upgrade || exit 11
 
 apt -y install \
     build-essential \
-    qt4-qmake qt4-dev-tools \
-    libcurl4-openssl-dev \
     git git-core gitk xclip \
     vim vim-gtk vim-runtime vim-scripts exuberant-ctags cscope \
     meld diffuse \
-    libssl-dev libc-ares-dev uuid-dev \
     cmake \
     monit \
     xsltproc docbook-xsl || exit 20
 
+    #qt4-qmake qt4-dev-tools \
+    #libcurl4-openssl-dev \
+    #libssl-dev libc-ares-dev uuid-dev \
 
 echo "Done..."
 exit 0
